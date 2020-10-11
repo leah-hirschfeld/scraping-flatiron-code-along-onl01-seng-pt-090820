7eb581b243c6f41f44d64ff9974eba63a4333214
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-11 16:25:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,15 @@ class Scraper
     end
   end
   
+  def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
+  end
+ 
 end
+ 
+Scraper.new.get_page
 
 
 
